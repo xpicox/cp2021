894a4e4618f72529b615769813c43d8d20f66ccd
valentino <valentino.picotti@gmail.com>
2021-04-21 15:40:09 +0200

    Fix formatting

--- exercises/src/main/java/cp/week14/ThreadsExercise10.java
+++ exercises/src/main/java/cp/week14/ThreadsExercise10.java
@@ -20,45 +20,45 @@ import cp.week10.Words;
  */
 public class ThreadsExercise10 {
 	/*
-	 * Modify ThreadsExercise9 to use Files.walk over the data directory in the
-	 * Threads project, such that you recursively visit all files in that directory
-	 * instead of using a fixed list of filenames.
+	 * Modify ThreadsExercise9 to use Files.walk over the data directory in the Threads project, such
+	 * that you recursively visit all files in that directory instead of using a fixed list of
+	 * filenames.
 	 */
 
 	public static void main() {
 		// word -> number of times that it appears over all files
-		Map<Character, Set<String>> occurrences = new ConcurrentHashMap<>();
-		List<Thread> threads = new ArrayList<>();
+		Map< Character, Set< String > > occurrences = new ConcurrentHashMap<>();
+		List< Thread > threads = new ArrayList<>();
 
 		try {
-			Files.walk(Paths.get(".")).filter(Files::isRegularFile)
-					.map(path -> new Thread(() -> computeWords(path, occurrences)))
-					.forEach(thread -> threads.add(thread));
-		} catch (IOException e) {
+			Files.walk( Paths.get( "." ) ).filter( Files::isRegularFile )
+				.map( path -> new Thread( () -> computeWords( path, occurrences ) ) )
+				.forEach( thread -> threads.add( thread ) );
+		} catch( IOException e ) {
 			e.printStackTrace();
 		}
 
-		threads.forEach(Thread::start);
-		threads.forEach(t -> {
+		threads.forEach( Thread::start );
+		threads.forEach( t -> {
 			try {
 				t.join();
-			} catch (InterruptedException e) {
+			} catch( InterruptedException e ) {
 				e.printStackTrace();
 			}
-		});
+		} );
 	}
 
-	private static void computeWords(Path path, Map<Character, Set<String>> occurrences) {
+	private static void computeWords( Path path, Map< Character, Set< String > > occurrences ) {
 		try {
-			Files.lines(path).flatMap(Words::extractWords).forEach(s -> {
-				Character c = s.charAt(0);
-				occurrences.merge(c, Collections.singleton(s), (s1, s2) -> {
-					Set<String> result = new HashSet<>(s1);
-					result.addAll(s2);
+			Files.lines( path ).flatMap( Words::extractWords ).forEach( s -> {
+				Character c = s.charAt( 0 );
+				occurrences.merge( c, Collections.singleton( s ), ( s1, s2 ) -> {
+					Set< String > result = new HashSet<>( s1 );
+					result.addAll( s2 );
 					return result;
-				});
-			});
-		} catch (IOException e) {
+				} );
+			} );
+		} catch( IOException e ) {
 			e.printStackTrace();
 		}
 	}
--- exercises/src/main/java/cp/week14/ThreadsExercise9.java
+++ exercises/src/main/java/cp/week14/ThreadsExercise9.java
@@ -17,48 +17,47 @@ import cp.week10.Words;
  *
  * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
  */
-public class ThreadsExercise9
-{
+public class ThreadsExercise9 {
 	/*
-	Modify Threads/cp/ConcurrentMap to compute a map of type Map<Character, Set<String>>.
-	The map should map a character to the set of words that start with that character (case sensitive).
-	*/
+	 * Modify Threads/cp/ConcurrentMap to compute a map of type Map<Character, Set<String>>. The map
+	 * should map a character to the set of words that start with that character (case sensitive).
+	 */
 
-	public static void main()
-	{
+	public static void main() {
 		// word -> number of times that it appears over all files
-		Map<Character, Set<String>> occurrences = new ConcurrentHashMap<>();
+		Map< Character, Set< String > > occurrences = new ConcurrentHashMap<>();
 
-		List<String> filenames = List.of("text1.txt", "text2.txt", "text3.txt", "text4.txt", "text5.txt", "text6.txt",
-				"text7.txt", "text8.txt", "text9.txt", "text10.txt");
+		List< String > filenames =
+			List.of( "text1.txt", "text2.txt", "text3.txt", "text4.txt", "text5.txt", "text6.txt",
+				"text7.txt", "text8.txt", "text9.txt", "text10.txt" );
 
-		CountDownLatch latch = new CountDownLatch(filenames.size());
+		CountDownLatch latch = new CountDownLatch( filenames.size() );
 
-		filenames.stream().map(filename -> new Thread(() -> {
-			computeWords(filename, occurrences);
+		filenames.stream().map( filename -> new Thread( () -> {
+			computeWords( filename, occurrences );
 			latch.countDown();
-		})).forEach(Thread::start);
+		} ) ).forEach( Thread::start );
 
 		try {
 			latch.await();
-		} catch (InterruptedException e) {
+		} catch( InterruptedException e ) {
 			e.printStackTrace();
 		}
 
-		//		occurrences.forEach( (word, n) -> System.out.println( word + ": " + n ) );
+		// occurrences.forEach( (word, n) -> System.out.println( word + ": " + n ) );
 	}
 
-	private static void computeWords(String filename, Map<Character, Set<String>> occurrences) {
+	private static void computeWords( String filename, Map< Character, Set< String > > occurrences ) {
 		try {
-			Files.lines(Paths.get(filename)).flatMap(Words::extractWords).forEach(s -> {
-				Character c = s.charAt(0);
-				occurrences.merge(c, Collections.singleton(s), (s1, s2) -> {
-						Set<String> result = new HashSet<>(s1);
-						result.addAll(s2);
-						return result;
-					});
-			});
-		} catch (IOException e) {
+			Files.lines( Paths.get( filename ) ).flatMap( Words::extractWords ).forEach( s -> {
+				Character c = s.charAt( 0 );
+				occurrences.merge( c, Collections.singleton( s ), ( s1, s2 ) -> {
+					Set< String > result = new HashSet<>( s1 );
+					result.addAll( s2 );
+					return result;
+				} );
+			} );
+		} catch( IOException e ) {
 			e.printStackTrace();
 		}
 	}
